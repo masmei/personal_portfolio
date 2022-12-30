33995292b5e3f95b43dc3a68183cc2b7e2595c1c
Mason Mei <masonmei@pursuit.org>
2022-12-29 23:20:10 -0500

    ready to deploy

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,9 @@ import {
 import { useState } from "react";
 import Image from "next/image";
 import avatar from "../public/avatar.jpeg";
-import code from "../public/code.png";
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
+import nftmarketplace from "../public/nftmarketplace.png";
+import littlepepper from "../public/littlepepper.png";
+import pursuitconnect from "../public/pursuitconnect.png";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(true);
@@ -27,7 +24,7 @@ export default function Home() {
 
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between">
+          <nav className="py-10 mb-12 flex justify-between border-b">
             <h1 className="text-xl font-burtons  dark:text-white">
               developedbymm
             </h1>
@@ -40,8 +37,9 @@ export default function Home() {
               </li>
               <li>
                 <a
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-lg ml-8"
-                  href="#"
+                  className="bg-gradient-to-r from-blue-500 to-blue-500 text-white px-4 py-2 rounded-lg ml-8"
+                  href="https://docs.google.com/document/d/1COB4ZrwQhKSpu5N0uMkg8vZr6n2ExjHP/edit"
+                  target="_blank"
                 >
                   Resume
                 </a>
@@ -80,9 +78,24 @@ export default function Home() {
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-3 text-black-600">
-            <AiFillTwitterCircle className=" dark:text-white" />
-            <AiFillLinkedin className=" dark:text-white" />
-            <AiFillGithub className=" dark:text-white" />
+            <a
+              href="https://twitter.com/Dapp_Boy"
+              target="_blank"
+            >
+              <AiFillTwitterCircle className=" dark:text-white" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/masonmei/"
+              target="_blank"
+            >
+              <AiFillLinkedin className=" dark:text-white" />
+            </a>
+            <a
+              href="https://github.com/masmei"
+              target="_blank"
+            >
+              <AiFillGithub className=" dark:text-white" />
+            </a>
           </div>
           <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
             <Image src={avatar} layout="fill" objectFit="cover" />
@@ -95,57 +108,123 @@ export default function Home() {
               My Projects
             </h2>
           </div>
-          <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-200">
-              <Image src={code} width={100} height={100} />
-              <h3 className="text-lg" font-medium pt-8 pb-2>
-                Title
-              </h3>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the <span className="text-teal-500">1500s</span>
-                , when an unknown printer took a galley of type and scrambled it
-                to make a type specimen book.
+          <div className="2xl:flex gap-10">
+            <div className="2xl:max-w-[31%] text-center shadow-lg p-2 rounded-xl my-10 py-5 dark:bg-gray-200">
+              <Image
+                className="rounded-xl"
+                src={nftmarketplace}
+                width={600}
+                height={375}
+              />
+              <h3 className="text-2xl py-2 font-bold">NFT Marketplace</h3>
+              <div className="py-2">
+                <a
+                  className="bg-gradient-to-r from-blue-500 to-blue-500 text-white px-4 py-2 rounded-lg mr-1"
+                  href="https://pursuitnfts.netlify.app/"
+                  target="_blank"
+                >
+                  Live Site
+                </a>
+
+                <a
+                  className="bg-gradient-to-r from-blue-500 to-blue-500 text-white px-4 py-2 rounded-lg ml-1"
+                  href="https://github.com/masmei/nft_marketplace"
+                  target="_blank"
+                >
+                  Github
+                </a>
+              </div>
+              <h4 className="text-xl pt-3 text-teal-600 font-semibold">
+                Tech Stack:
+              </h4>
+              <p className="text-gray-800 py-1 text-lg font-semibold italic">
+                Solidity | Ethers.js | React | Javascript | Bootstrap | Express
+                | PSQL{" "}
               </p>
-              <h4 className="py-4 text-teal-600">Tools I used</h4>
-              <p className="text-gray-800 py-1">Javascript</p>
-              <p className="text-gray-800 py-1">Javascript</p>
-              <p className="text-gray-800 py-1">Javascript</p>
+
+              <p className="px-10">
+                NFT Marketplace created with React. It has web3 integration,
+                connected to the Ethereum main-net and smart contracts written
+                with Solidity. Users are able to Create, Buy and Sell NFTs
+                directly on the app as well as connect with other users.
+              </p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-200">
-              <Image src={code} width={100} height={100} />
-              <h3 className="text-lg" font-medium pt-8 pb-2>
-                Title
+            <div className="2xl:max-w-[31%] text-center shadow-lg p-2 rounded-xl my-10 py-5 dark:bg-gray-200">
+              <Image
+                className="rounded-xl"
+                src={littlepepper}
+                width={600}
+                height={375}
+              />
+              <h3 className="text-2xl  py-2 font-bold">
+                Recipe Finder | Health Tracker
               </h3>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the <span className="text-teal-500">1500s</span>
-                , when an unknown printer took a galley of type and scrambled it
-                to make a type specimen book.
+              <div className="py-2">
+                <a
+                  className="bg-gradient-to-r from-blue-500 to-blue-500 text-white px-4 py-2 rounded-lg mr-1"
+                  href="https://littlepepper.netlify.app/"
+                  target="_blank"
+                >
+                  Live Site
+                </a>
+
+                <a
+                  className="bg-gradient-to-r from-blue-500 to-blue-500 text-white px-4 py-2 rounded-lg ml-1"
+                  href="https://github.com/masmei/A-Little-Pepper-FE"
+                  target="_blank"
+                >
+                  Github
+                </a>
+              </div>
+              <h4 className="text-xl pt-3 text-teal-600 font-semibold">
+                Tech Stack:
+              </h4>
+              <p className="text-gray-800 py-1 text-lg font-semibold italic">
+                React | Javascript | Bootstrap | Express | PSQL | API
               </p>
-              <h4 className="py-4 text-teal-600">Tools I used</h4>
-              <p className="text-gray-800 py-1">Javascript</p>
-              <p className="text-gray-800 py-1">Javascript</p>
-              <p className="text-gray-800 py-1">Javascript</p>
+              <p className="px-10">
+                Recipe finder and Health Tracker built with React using the
+                spoonacular API. This app aims to help users find recipes with
+                their desired ingredients to help them with healthier homemade
+                meals, less food wastage and keep track of their consumption
+                with the built in nutrition tracker.
+              </p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-200">
-              <Image src={code} width={100} height={100} />
-              <h3 className="text-lg" font-medium pt-8 pb-2>
-                Title
+            <div className="2xl:max-w-[31%] text-center shadow-lg p-2 rounded-xl my-10 py-5 dark:bg-gray-200">
+              <Image
+                className="rounded-xl"
+                src={pursuitconnect}
+                width={600}
+                height={375}
+              />
+              <h3 className="text-2xl  py-2 font-bold">
+                Social Networking App
               </h3>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the <span className="text-teal-500">1500s</span>
-                , when an unknown printer took a galley of type and scrambled it
-                to make a type specimen book.
+              <div className="py-2">
+                <a
+                  className="bg-gradient-to-r from-blue-500 to-blue-500 text-white px-4 py-2 rounded-lg mr-1"
+                  href="https://masonmei.wixsite.com/pursuitconnect"
+                  target="_blank"
+                >
+                  Live Site
+                </a>
+
+                <h4 className="text-xl pt-3 text-teal-600 font-semibold">
+                  Tech Stack:
+                </h4>
+                <p className="text-gray-800 py-1 text-lg font-semibold italic">
+                  Velo | Wix
+                </p>
+              </div>
+              <p className="px-10">
+                1st Place Winner for Pursuit Hackathon hosted by Major League
+                Hacking. With a team of 5 over a span of a weekend we created
+                this social networking app for Pursuit fellows and Recruiters
+                using Velo by Wix. Features include forums, job postings, news
+                feed, live chat, online courses, community events as well as
+                gamified profiles using badges earned through achievements
+                within the application.
               </p>
-              <h4 className="py-4 text-teal-600">Tools I used</h4>
-              <p className="text-gray-800 py-1">Javascript</p>
-              <p className="text-gray-800 py-1">Javascript</p>
-              <p className="text-gray-800 py-1">Javascript</p>
             </div>
           </div>
         </section>
@@ -217,7 +296,7 @@ export default function Home() {
             </div>
           </div>
         </section> */}
-        <section className="text-center shadow-lg p-10 rounded-xl my-10">
+        <section className="text-center border-t p-10 my-10">
           <div className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl font-burtons  dark:text-white">
               developedbymm
@@ -233,9 +312,24 @@ export default function Home() {
             </div>
           </div>
           <div className="text-4xl flex justify-center gap-16 text-black-600  dark:text-white">
-            <AiFillTwitterCircle />
-            <AiFillLinkedin />
-            <AiFillGithub />
+            <a
+              href="https://twitter.com/Dapp_Boy"
+              target="_blank"
+            >
+              <AiFillTwitterCircle />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/masonmei/"
+              target="_blank"
+            >
+              <AiFillLinkedin />
+            </a>
+            <a
+              href="https://github.com/masmei"
+              target="_blank"
+            >
+              <AiFillGithub />
+            </a>
           </div>
         </section>
       </main>
